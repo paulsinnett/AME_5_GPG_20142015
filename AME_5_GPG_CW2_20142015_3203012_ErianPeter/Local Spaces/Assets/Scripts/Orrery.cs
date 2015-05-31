b865895f0a9f4d3fa2b48c1b973308565ed60932
Paul Sinnett <paul.sinnett@gmail.com>
2015-05-31 23:27:09 +0100

    Peter
    original files

---
+++ AME_5_GPG_CW2_20142015_3203012_ErianPeter/Local Spaces/Assets/Scripts/Orrery.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orrery : MonoBehaviour {
+
+	public Transform sun;
+	public Transform earth;
+	public Transform Moon;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Moon.transform.RotateAround (earth.transform.position, Vector3.up, 365 * Time.deltaTime);
+		earth.transform.RotateAround (sun.transform.position, Vector3.up, 30 * Time.deltaTime);
+
+		earth.transform.localRotation = Quaternion.Euler (Vector3.up);
+		Moon.transform.localRotation = Quaternion.Euler (Vector3.up);
+	}
+}
---
+++ AME_5_GPG_CW2_20142015_3203012_ErianPeter/NumberSheeps/Assets/Scripts/NumberSheeps.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberSheeps : MonoBehaviour 
+{
+
+	public string[] OldEnglishB20;
+	
+	public Text tSheepCount;
+	public Text tMarbleCount;
+	public Text tTotalSheepCount;
+	
+	public int iSheepCount;
+	public int iMarbleCount;
+	public int iTotalSheepCount;
+
+	void Update () 
+	{
+		DisplayText ();
+	}
+	
+
+	void DisplayText ()
+	{
+		tSheepCount.text = "Sheep Count:" + " " + OldEnglishB20 [iSheepCount];
+		tMarbleCount.text = "Marble Count:" + " " + iMarbleCount.ToString ("0");
+		tTotalSheepCount.text = "Total Sheep Count:" + " " + iTotalSheepCount.ToString ("0");
+
+
+	}
+
+	public void PressButton()
+	{
+		if (iSheepCount < 20) 
+		{
+			iSheepCount++;
+		} 
+		else 
+		{
+			iMarbleCount++;
+			iSheepCount = 0 ;
+		}
+		if (iSheepCount != 0) 
+		{
+			iTotalSheepCount++;
+		}
+
+	}
+
+}
