afed61743ab7f042a5f66963b79884c71a93af45
Paul Sinnett <paul.sinnett@gmail.com>
2015-07-20 19:34:27 +0100

    Charlie Carter

---
+++ AME_5_GPG_CW2_20142015_3208097_CarterCharlie/Assets/Standard Assets/Scripts/Enemy/EnemyAI_Patrol.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI_Patrol : MonoBehaviour 
+{
+	public Transform[] Waypoints;
+	public float Speed;
+	public int curWayPoint;
+	public bool doPatrol = true;
+	public Vector3 Target;
+	public Vector3 MoveDirection;
+	public Vector3 Velocity;
+
+	void Update ()
+	{
+		if (curWayPoint < Waypoints.Length) 
+		{
+						Target = Waypoints[curWayPoint].position;
+						MoveDirection = Target - transform.position;
+						Velocity = GetComponent<Rigidbody>().velocity;
+
+						if (MoveDirection.magnitude < 1) 
+			{
+								curWayPoint++;
+						} 
+			else 
+			{
+								Velocity = MoveDirection.normalized * Speed;
+						}
+				}
+		else
+		{				
+				if(doPatrol)
+			{
+				curWayPoint=0;
+			}
+			else
+			{
+				Velocity = Vector3.zero;
+			}
+		}
+		GetComponent<Rigidbody>().velocity = Velocity;
+		transform.LookAt(Target);
+	}
+}
---
+++ AME_5_GPG_CW2_20142015_3208097_CarterCharlie/Assets/Standard Assets/Scripts/Pickup/GameOver.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var labelText : String = "You have won";
+
+function OnGUI() {
+GUILayout.Label(labelText);
+}
+function OnTriggerEnter( other : Collider ){
+ 
+ if (other.tag == "Player") {
+	labelText = "You have successfully completed the objective of touching the sacred capsule, cheeky! Now feel free to run around for as long as you like. But for goodness sake don't let the guard see you.";
+    
+    }
+ 
+
+}
